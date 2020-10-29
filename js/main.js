e50d9095861e93a4d4c3b7e35f244824df55f065
nalezhytyi <kawory05@gmail.com>
2020-10-29 10:57:18 +0200

    added favicon

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 //SHOW MENU
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.menu');
-  const menuItem = document.querySelectorAll('.menu li');
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.menu');
+    const menuItem = document.querySelectorAll('.menu li');
 
-  /* Toggle Nav */
-  const showMenu = () => {
-    nav.classList.toggle('show');
+    /* Toggle Nav */
+    const showMenu = () => {
+        nav.classList.toggle('show');
 
-    /* Animate Links */
-    menuItem.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-      }
-    });
+        /* Animate Links */
+        menuItem.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
 
-    /* Burger Animation */
-    burger.classList.toggle('toggle');
-  };
+        /* Burger Animation */
+        burger.classList.toggle('toggle');
+    };
 
-  burger.addEventListener('click', showMenu);
+    burger.addEventListener('click', showMenu);
 };
 
 navSlide();
@@ -30,48 +30,50 @@ navSlide();
 const menuLink = document.querySelectorAll('.menu__link');
 
 function linkAction() {
-  /*Active link*/
-  menuLink.forEach((n) => n.classList.remove('active'));
-  this.classList.add('active');
+    /*Active link*/
+    menuLink.forEach((n) => n.classList.remove('active'));
+    this.classList.add('active');
 }
 
 menuLink.forEach((n) => n.addEventListener('click', linkAction));
 
 //SMOOTH SCROLL AFTER CLICK ON ANCHOR LINK
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth',
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+        });
     });
-  });
 });
 
 //SLIDER
 document.addEventListener('DOMContentLoaded', function () {
-  new Splide('.splide', {
-    type: 'loop',
-    autoplay: true,
-    pauseOnHover: false,
-    arrows: false,
-  }).mount();
+    new Splide('.splide', {
+        type: 'loop',
+        autoplay: true,
+        pauseOnHover: false,
+        arrows: false,
+    }).mount();
 });
 
 //PRELOADER
 const hellopreloader = document.getElementById('hellopreloader_preload');
+
 function fadeOutnojquery(el) {
-  el.style.opacity = 1;
-  var interhellopreloader = setInterval(function () {
-    el.style.opacity = el.style.opacity - 0.05;
-    if (el.style.opacity <= 0.05) {
-      clearInterval(interhellopreloader);
-      hellopreloader.style.display = 'none';
-    }
-  }, 16);
+    el.style.opacity = 1;
+    const interhellopreloader = setInterval(() => {
+        el.style.opacity = el.style.opacity - 0.05;
+        if (el.style.opacity <= 0.05) {
+            clearInterval(interhellopreloader);
+            hellopreloader.style.display = 'none';
+        }
+    }, 16);
 }
+
 window.onload = function () {
-  setTimeout(function () {
-    fadeOutnojquery(hellopreloader);
-  }, 1000);
+    setTimeout(function () {
+        fadeOutnojquery(hellopreloader);
+    }, 1000);
 };
